5e2ae1393d267a99a52f08a293fe3104ed8bb0f0
aaronjenson <aaronajenson@gmail.com>
2016-02-02 16:56:04 -0800

    Added some of the base methods
    Created idle method and constructor as well as started work within them.

--- src/org/usfirst/frc/team3238/robot/CollectAndShoot.java
+++ src/org/usfirst/frc/team3238/robot/CollectAndShoot.java
@@ -1,6 +1,105 @@
 package org.usfirst.frc.team3238.robot;
 
+import edu.wpi.first.wpilibj.CANTalon;
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.DriverStation;
+
+enum State
+{
+    WAITING, CENTERING, HOLDING, SHOOTING, DISABLED
+}
+
+enum ShooterSubState
+{
+    WARMINGUP, SHOOTING, DISABLED
+}
+
 public class CollectAndShoot
 {
+    State state;
+    ShooterSubState shooterSubState;
 
+    CANTalon collectorTalon;
+    CANTalon shooterTalonOne;
+    CANTalon shooterTalonTwo;
+    DigitalInput collectSwitch;
+    DigitalInput centerSwitch;
+    DigitalInput holdPosSwitch;
+    Joystick stick;
+    Timer timer;
+
+    public CollectAndShoot(int collectorTalonID, int shooterTalonOneID,
+            int shooterTalonTwoID, int collectSwitchChannel,
+            int centerSwitchChannel, int holdPosSwitchChannel, int stickPort)
+    {
+        collectorTalon = new CANTalon(collectorTalonID);
+        shooterTalonOne = new CANTalon(shooterTalonOneID);
+        shooterTalonTwo = new CANTalon(shooterTalonTwoID);
+
+        collectSwitch = new DigitalInput(collectSwitchChannel);
+        centerSwitch = new DigitalInput(centerSwitchChannel);
+        holdPosSwitch = new DigitalInput(holdPosSwitchChannel);
+
+        stick = new Joystick(stickPort);
+
+        state = State.WAITING;
+
+        timer.start();
+    }
+
+    public void idle()
+    {
+        switch(state)
+        {
+            case WAITING:
+                collectorTalon.set(1.0);
+                if(collectSwitch.get())
+                    state = State.CENTERING;
+                break;
+            case CENTERING:
+                collectorTalon.set(0.8);
+                if(centerSwitch.get())
+                    state = State.HOLDING;
+                break;
+            case DISABLED:
+                break;
+            case HOLDING:
+                if(collectSwitch.get() && !holdPosSwitch.get()
+                        && !centerSwitch.get())
+                    collectorTalon.set(0.5);
+                else if(centerSwitch.get() && !holdPosSwitch.get()
+                        && !collectSwitch.get())
+                    collectorTalon.set(-0.5);
+                else if(holdPosSwitch.get())
+                    collectorTalon.set(0.0);
+                else if(!collectSwitch.get() && !centerSwitch.get()
+                        && !holdPosSwitch.get())
+                    state = State.WAITING;
+                else
+                    DriverStation
+                            .reportError(
+                                    "Your collector limit switches are seriously messed up!",
+                                    false);
+
+                if(stick.getRawButton(1))
+                {
+                    state = State.SHOOTING;
+                    timer.reset();
+                    timer.start();
+                }
+                break;
+            case SHOOTING:
+                if(stick.getRawButton(1))
+                {
+                    shooterTalonOne.set(0.85);
+                    shooterTalonTwo.set(-0.85);
+                }
+                break;
+            default:
+                break;
+
+        }
+    }
 }
