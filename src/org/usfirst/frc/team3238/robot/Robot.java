55214448dd78cefeecefeeeb1e46a47403c2c40a
aaronjenson <aaronajenson@gmail.com>
2016-02-06 14:13:12 -0800

    Small changes to collectandshoot constructor

--- src/org/usfirst/frc/team3238/robot/Robot.java
+++ src/org/usfirst/frc/team3238/robot/Robot.java
@@ -7,70 +7,62 @@ import edu.wpi.first.wpilibj.CANTalon;
 
 public class Robot extends IterativeRobot
 {
+    Breacher breacherArm;
     Camera camera;
     Chassis chassis;
     CollectAndShoot ballControl;
     Joystick joystickZero, joystickOne;
     CANTalon leftDriveTalonA, leftDriveTalonB, rightDriveTalonA,
-            rightDriveTalonB, breacherTalon, collectorTalon, shooterTalonLeft,
-            shooterTalonRight;
+            rightDriveTalonB;
+    CANTalon breacherTalon;
+    CANTalon collectorTalon;
+    CANTalon shooterTalonLeft, shooterTalonRight;
     DigitalInput armDetectTop, armDetectBot;
-    Breacher breacherArm;
+    DigitalInput collectSwitch, centerSwitch, holdingSwitch;
     public double throttleRangeAdjuster;
     public static boolean camChanging;
     public static boolean camDead;
 
-    public void defineConstants() throws java.io.FileNotFoundException
-    {
-
-    }
-
     public void robotInit()
     {
-        try
-        {
-            armDetectTop = new DigitalInput(Constants.Breacher.armDetectTopPort);
-            armDetectBot = new DigitalInput(Constants.Breacher.armDetectBotPort);
+        joystickZero = new Joystick(Constants.Robot.joystickZeroPort);
+        joystickOne = new Joystick(Constants.Robot.joystickOnePort);
 
-            leftDriveTalonA = new CANTalon(Constants.Chassis.leftMotorOneID);
-            leftDriveTalonB = new CANTalon(Constants.Chassis.leftMotorTwoID);
-            rightDriveTalonA = new CANTalon(Constants.Chassis.rightMotorOneID);
-            rightDriveTalonB = new CANTalon(Constants.Chassis.rightMotorTwoID);
+        armDetectTop = new DigitalInput(Constants.Breacher.armDetectTopPort);
+        armDetectBot = new DigitalInput(Constants.Breacher.armDetectBotPort);
+        collectSwitch = new DigitalInput(
+                Constants.CollectAndShoot.ballDetectChannel);
+        centerSwitch = new DigitalInput(
+                Constants.CollectAndShoot.centerDetectChannel);
+        holdingSwitch = new DigitalInput(
+                Constants.CollectAndShoot.holdPosDetectChannel);
 
-            breacherTalon = new CANTalon(Constants.Breacher.breacherTalonPort);
+        leftDriveTalonA = new CANTalon(Constants.Chassis.leftMotorOneID);
+        leftDriveTalonB = new CANTalon(Constants.Chassis.leftMotorTwoID);
+        rightDriveTalonA = new CANTalon(Constants.Chassis.rightMotorOneID);
+        rightDriveTalonB = new CANTalon(Constants.Chassis.rightMotorTwoID);
+        breacherTalon = new CANTalon(Constants.Breacher.breacherTalonPort);
+        collectorTalon = new CANTalon(
+                Constants.CollectAndShoot.collectorTalonPort);
+        shooterTalonLeft = new CANTalon(
+                Constants.CollectAndShoot.shooterLeftTalonPort);
+        shooterTalonRight = new CANTalon(
+                Constants.CollectAndShoot.shooterRightTalonPort);
 
-            breacherArm = new Breacher(breacherTalon, armDetectTop,
-                    armDetectBot);
-            chassis = new Chassis(leftDriveTalonA, leftDriveTalonB,
-                    rightDriveTalonA, rightDriveTalonB);
+        breacherArm = new Breacher(breacherTalon, armDetectTop, armDetectBot);
+        camera = new Camera(Constants.Camera.frontCamName,
+                Constants.Camera.rearCamName,
+                Constants.Camera.crosshairCenterX,
+                Constants.Camera.crosshairCenterY);
+        camera.init(Constants.Camera.camQuality, Constants.Camera.camSize);
+        camDead = false;
+        chassis = new Chassis(leftDriveTalonA, leftDriveTalonB,
+                rightDriveTalonA, rightDriveTalonB);
+        ballControl = new CollectAndShoot(collectorTalon, shooterTalonLeft,
+                shooterTalonRight, collectSwitch, centerSwitch, holdingSwitch,
+                joystickZero);
 
-            breacherArm = new Breacher(breacherTalon);
-
-            camera = new Camera(Constants.Camera.frontCamName,
-                    Constants.Camera.rearCamName,
-                    Constants.Camera.crosshairCenterX,
-                    Constants.Camera.crosshairCenterY);
-            camera.init(Constants.Camera.camQuality, Constants.Camera.camSize);
-            camDead = false;
-
-            ballControl = new CollectAndShoot(
-                    Constants.CollectAndShoot.collectorTalonPort,
-                    Constants.CollectAndShoot.shooterLeftTalonPort,
-                    Constants.CollectAndShoot.shooterRightTalonPort,
-                    Constants.CollectAndShoot.ballDetectChannel,
-                    Constants.CollectAndShoot.centerDetectChannel,
-                    Constants.CollectAndShoot.holdPosDetectChannel,
-                    Constants.Robot.joystickZeroPort);
-
-            throttleRangeAdjuster = Constants.Robot.throttleRangeAdjuster;
-
-            joystickZero = new Joystick(Constants.Robot.joystickZeroPort);
-            joystickOne = new Joystick(Constants.Robot.joystickOnePort);
-        } catch(Exception e)
-        {
-            e.printStackTrace();
-        }
-
+        throttleRangeAdjuster = Constants.Robot.throttleRangeAdjuster;
     }
 
     public void autonomousInit()
