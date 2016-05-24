9408e8ac35afcc2f909644f4ef7782ba1e7acc5b
KangarooMan64 <smithgar002@gmail.com>
2016-05-24 10:48:00 -0700

    PIController Doc

--- src/org/usfirst/frc/team3238/robot/PIController.java
+++ src/org/usfirst/frc/team3238/robot/PIController.java
@@ -3,17 +3,24 @@ package org.usfirst.frc.team3238.robot;
 /**
  * This is a generic proportional integral controller.
  * 
- * @author Team 3238
+ * @author FRC Team 3238
  */
 public class PIController
 {
-    double m_cummulativeError = 0;
+    double m_cummulativeError = 0; //ignore the extra 'm'
     double m_oldTime = 0;
     double m_pConstant;
     double m_iConstant;
     double m_error;
     double m_throttle;
 
+    /**
+     * PI Constructor
+     * 
+     * @param pConstant the 'proportionality' constant
+     * @param iConstant the 'integral' constant
+     * @param error the current error
+     */ 
     PIController(double pConstant, double iConstant, double error)
     {
         m_pConstant = pConstant;
@@ -63,13 +70,8 @@ public class PIController
 
     boolean isAligned(double setPoint, double sensorValue)
     {
-        if(Math.abs(setPoint - sensorValue) < m_error)
-        {
-            return true;
-        } else
-        {
-            return false;
-        }
+        return Math.abs(setPoint - sensorValue) < m_error
+        
     }
 
     /**
@@ -81,17 +83,29 @@ public class PIController
         m_oldTime = 0;
     }
 
+    /**
+     * mutator method for the proportionality and integral constants
+     */ 
     void inputConstants(double pConstant, double iConstant)
     {
         m_pConstant = pConstant;
         m_iConstant = iConstant;
     }
-
+    /**
+     * mutator method for throttle which is multiplied by the motor speed to change turn power
+     */ 
     void setThrottle(double throttle)
     {
         m_throttle = throttle;
     }
-
+    /**
+     * Pretty complicated and unnecessary math but in the end returns a value that is between -1.0 and 1.0 although it doesn't matter, 
+     * because it's used to utilize motor power in the scope of the allowed power assignment but extending beyond this scope will
+     * cause it to be limited anyway and is less useful for debugging.
+     * 
+     * @param motorPower the current motor power
+     * @return returnPower the motor power within -1.0<=x<=1.0
+     */ 
     double limitOutput(double motorPower)
     {
         double returnPower;
