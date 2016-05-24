cdf39685ae514f34928cde23ea712acbd407b55d
KangarooMan64 <smithgar002@gmail.com>
2016-05-24 12:49:20 -0700

    Vision doc'd, baseline

--- src/org/usfirst/frc/team3238/robot/Vision.java
+++ src/org/usfirst/frc/team3238/robot/Vision.java
@@ -61,12 +61,14 @@ public class Vision
         pidY = new PIController(Constants.Vision.pValueY,
                 Constants.Vision.iValueY, Constants.Vision.errorY);
         pidY.setThrottle(Constants.Vision.throttleY);
-        netTab = NetworkTable.getTable("GRIP/Contours");
+        netTab = NetworkTable.getTable("GRIP/Contours"); //gets vision processing w/ grip output
         chassisY = 0.0;
         chassisTwist = 0.0;
         stop();
     }
-
+    /**
+     * Resets dynamic values and enables subsystems. (Not sure why it's called stop, more like reset)
+     */ 
     void stop()
     {
         shooter.init();
@@ -77,12 +79,20 @@ public class Vision
         pidY.reinit();
     }
 
+    /**
+     * Makes the movespeed (chassisY) and turnspeed (chassisTwist) equal to 0, thus stopping chassis/drivesystem/wheel movement
+     */ 
     void stopChassis()
     {
         chassisY = 0.0;
         chassisTwist = 0.0;
     }
-
+    /**
+     * Starts shooter, doesn't start if collector is collecting
+     * sets the RPM to an optimized speed for a static distance
+     * turns on collector mode
+     * moves chassis with chassisY and chassisTwist
+     */ 
     void run()
     {
         shooter.idle(collector.isCollecting());
@@ -91,23 +101,30 @@ public class Vision
         collector.idle();
         chassis.arcadeDriveAuto(chassisY, chassisTwist);
     }
-
+    /**
+     * used in teleop- doesn't change RPM because assume driver has the right RPM for the distance and doesn't use collector because
+     * there is already a ball, tries to align with goal
+     */ 
     void runTele()
     {
         shooter.idle(collector.isCollecting());
         collector.idle();
         chassis.arcadeDriveAuto(chassisY, chassisTwist);
     }
-
+    /**
+     * gets and sets the X and Y value of the 'contour' (detected shape)
+     * 
+     * @return ret true if X and Y are set to a dynamic value and the tower is detected.
+     */ 
     boolean getTowerPos()
     {
         boolean ret;
         try
         {
-            x = netTab.getNumberArray("centerX", defaultX);
+            x = netTab.getNumberArray("centerX", defaultX); 
             y = netTab.getNumberArray("centerY", defaultY);
             ret = true;
-        } catch(Exception e)
+        } catch(Exception e) //if defaultX and defaultY have no indexes then set X and Y to default values
         {
             x = defaultX;
             y = defaultY;
@@ -122,15 +139,17 @@ public class Vision
 
         return ret;
     }
-
+    /**
+     * Aligns with the tower in tele-op
+     */ 
     public void teleVision()
     {
         try
         {
-            if(!getTowerPos() && enabled)
+            if(!getTowerPos() && enabled) //if trying to tower align but tower is not detected, stop he chassis
             {
                 stopChassis();
-            } else if(!pidY.isAligned(Constants.Vision.setPointY, y[0]))
+            } else if(!pidY.isAligned(Constants.Vision.setPointY, y[0])) //If enabled but y is not accurate, do some PI stuff to align 
             {
                 chassisTwist = -pidY.getMotorValue(Constants.Vision.setPointY,
                         y[0]);
@@ -148,7 +167,7 @@ public class Vision
                         x[0]);
                 timer.reset();
                 timer.start();
-            } else if(!pidX.isAligned(Constants.Vision.setPointX, x[0]))
+            } else if(!pidX.isAligned(Constants.Vision.setPointX, x[0])) //same as above, just with X
             {
                 chassisY = -pidX.getMotorValue(Constants.Vision.setPointX,
                         x[0]);
@@ -184,16 +203,20 @@ public class Vision
             DriverStation.reportError(e.getMessage(), true);
         }
     }
-    
+    /**
+     * Does some basic turning alignment to help for less error when the PI loop starts, based on the defense crossed in autonomous
+     * 
+     * @param position the position of the defense crossed in autonomous
+     */ 
     public void sweepX(int position)
     {
         switch(position)
         {
             case 0:
-                chassis.disable();
+                chassis.disable(); //chassis didn't cross anything
                 break;
             case 1:
-                chassis.arcadeDriveAuto(0.0, -0.6);
+                chassis.arcadeDriveAuto(0.0, -0.6); //turn a bit to align better
                 break;
             case 2:
                 chassis.arcadeDriveAuto(0.0, -0.6);
@@ -209,14 +232,16 @@ public class Vision
                 break;
         }
     }
-    
+    /**
+     * Does some random chassis movement action to try to get a rough alignment
+     */ 
     public void sweepX()
     {
         switch(control)
         {
             case "left":
                 chassis.arcadeDriveAuto(0.0, 0.5);
-                if(timer.get() >= Math.random())
+                if(timer.get() >= Math.random()) //2lazy4PI, just use math.random
                 {
                     control = "resetRight";
                 }
@@ -233,7 +258,7 @@ public class Vision
                 break;
             case "right":
                 chassis.arcadeDriveAuto(0.0, 0.5);
-                if(timer.get() >= Math.random() * -1)
+                if(timer.get() >= Math.random() * -1) //why is it -1?
                 {
                     control = "resetRight";
                 }
@@ -241,19 +266,27 @@ public class Vision
         }
     }
     
+    /**
+     * Drives backwards if bot is too close
+     */ 
     public void sweepY()
     {
         chassis.arcadeDriveAuto(-0.4, 0.0);
     }
 
+    /**
+     * Moves and aligns bot to shoot, same functionality as teleVision() except designed for autonomous so more control with auto
+     * 
+     * @param pos the position of he defense crossed in autonomous
+     */ 
     public void idle(int pos)
     {
         try
         {
-            if(!getTowerPos() && enabled)
+            if(!getTowerPos() && enabled) //tower not detected but enabled, stop chassis
             {
                 stopChassis();
-            } else if(!pidY.isAligned(Constants.Vision.setPointY, y[0]))
+            } else if(!pidY.isAligned(Constants.Vision.setPointY, y[0])) //if tower detected but not Y aligned use PI to align fwd/bwd
             {
                 targetFound = true;
                 chassisTwist = -pidY.getMotorValue(Constants.Vision.setPointY,
@@ -267,7 +300,7 @@ public class Vision
                         x[0]);
                 timer.reset();
                 timer.start();
-            } else if(!pidX.isAligned(Constants.Vision.setPointX, x[0]))
+            } else if(!pidX.isAligned(Constants.Vision.setPointX, x[0])) //same as above, with x
             {
                 targetFound = true;
                 chassisY = -pidX.getMotorValue(Constants.Vision.setPointX,
@@ -282,10 +315,10 @@ public class Vision
                 timer.reset();
                 timer.start();
             } else if(pidX.isAligned(Constants.Vision.setPointX, x[0])
-                    && pidY.isAligned(Constants.Vision.setPointY, y[0]))
+                    && pidY.isAligned(Constants.Vision.setPointY, y[0])) //If both are aligned then start up the shooter
             {
                 if((timer.get() > 1.0)
-                        && shooter.isRPMReached(Constants.Vision.shooterSpeed))
+                        && shooter.isRPMReached(Constants.Vision.shooterSpeed)) //if the RPM is fast enough to shoot ball into the goal, and it's been 1second, shoot
                 {
                     collector.shoot();
                     enabled = false;
